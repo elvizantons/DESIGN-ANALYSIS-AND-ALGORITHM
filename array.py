579c2b51ecfe4464c3b3c8b1df19ea215a3c06b9
elvizantons <elvizantons.cse2025@citchennai.net>
2026-06-29 11:09:38 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,14 @@
+# Input array
+arr = list(map(int, input("Enter array elements: ").split()))
+
+# Choice
+choice = int(input("Enter 1 for Ascending, 2 for Descending: "))
+
+if choice == 1:
+    arr.sort()
+    print("Ascending order:", arr)
+elif choice == 2:
+    arr.sort(reverse=True)
+    print("Descending order:", arr)
+else:
+    print("Invalid choice")
